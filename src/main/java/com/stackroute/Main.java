fe1db74ddc6366608a897eb693f4d311688d8b70
srija <srijak890@gmail.com>
2019-07-11 16:30:41 +0530

    spring-xml-demo

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,39 @@
+package com.stackroute;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+public class Main
+{
+    public  static void main(String[] args)
+    {
+        //Using application context
+        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("bean.xml file loaded");
+        Movie movie=context.getBean("movie",Movie.class);
+        movie.display();
+
+        //Using BeenFactory
+        ClassPathResource res = new ClassPathResource("beans.xml");
+        BeanFactory factory = new XmlBeanFactory(res);
+        Movie movie1=context.getBean("movie",Movie.class);
+        movie1.display();
+
+        //Using BeanDefinationReader and BeanDefinationReader
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        BeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
+        Movie movie2= (Movie) beanFactory.getBean("movie");
+        movie2.display();
+
+    }
+
+}
---
+++ src/main/java/com/stackroute/Movie.java
@@ -0,0 +1,24 @@
+package com.stackroute;
+
+public class Movie {
+    private String m_name;
+    Actor actor; //creating object
+
+    //setter and getter methods
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    public String getM_name() {
+        return m_name;
+    }
+
+    public void setM_name(String m_name) {
+        this.m_name = m_name;
+    }
+
+    public  void display(){
+        System.out.println(""+actor.getName() +""+actor.getGender() +""+actor.getAge());
+    }
+
+}
