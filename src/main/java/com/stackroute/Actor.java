d609b73f866799d787710e1e834ff7a92e8d6c95
Srija890 <51942950+Srija890@users.noreply.github.com>
2019-07-18 20:15:13 +0530

    Update Actor.java

--- src/main/java/com/stackroute/Actor.java
+++ src/main/java/com/stackroute/Actor.java
@@ -1,30 +1,30 @@
 package com.stackroute;
 
 public class Actor {
-    private String name;
-    private String gender;
-    private  int age;
+    private String aname;
+    private String agender;
+    private  int aage;
 
     //setter and getter methods
-    public String getName() {
+    public String getAName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setAName(String aname) {
+        this.aname = aname;
     }
 
-    public String getGender() {
-        return gender;
+    public String getAGender() {
+        return agender;
     }
-    public void setGender(String gender) {
-        this.gender = gender;
+    public void setAGender(String agender) {
+        this.agender = agender;
     }
 
-    public int getAge() {
-        return age;
+    public int getAAge() {
+        return aage;
     }
-    public void setAge(int age) {
-        this.age = age;
+    public void setAAge(int aage) {
+        this.aage = aage;
     }
 
 }
